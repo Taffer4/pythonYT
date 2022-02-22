d3064195811b0ccff15bedd3e266bf5afa130ea4
taffer4 <taffer4@outlook.com>
2022-02-22 20:50:38 +0300

    Added functionality to retrieve full list of videos from channel

--- flask_app.py
+++ flask_app.py
@@ -19,43 +19,65 @@ app = Flask(__name__)
 
 @app.route('/')
 
-def getVideos():
+def getVideosDataFromYT(nextPageToken):
     #Setting YouTube API
     api_key = "AIzaSyDReRsRej0OfdaOUOfq5z_PHvqqKepIDLU"
     api_service_name = "youtube"
     api_version = "v3"
 
     client = build(api_service_name, api_version, developerKey=api_key)
-    request = client.search().list(
-    	part="snippet",
-    	channelId="UCppy4jafHu51iCMl-7qVbFA",
-    	type="video",
-    	maxResults=50,
-        order='date'
-    )
+
+    if(nextPageToken == None):
+        request = client.search().list(
+            part="snippet",
+            channelId="UCppy4jafHu51iCMl-7qVbFA",
+            type="video",
+            maxResults=50,
+            order ='date'
+        )
+    else:
+        request = client.search().list(
+            part="snippet",
+            channelId="UCppy4jafHu51iCMl-7qVbFA",
+            type="video",
+            maxResults=50,
+            order ='date',
+            pageToken = nextPageToken
+        )
+
 
     #Requesting data from YT
     json_data = request.execute()
-    dumped_json = json.dumps(json_data)
-    user = User(datetime.today(), json_data)
+    #dumped_json = json.dumps(json_data)
+    #user = User(datetime.today(), json_data)
 
     #Database stuff
     #Here we create engine
-    engine = create_engine('mysql://Taffer:189gk642@Taffer.mysql.pythonanywhere-services.com/Taffer$test_youtubue')
+    #engine = create_engine('mysql://Taffer:189gk642@Taffer.mysql.pythonanywhere-services.com/Taffer$test_youtubue')
 
     #Now the session goes up!
-    Session = sessionmaker(bind = engine)
+    #Session = sessionmaker(bind = engine)
 
-    session = Session()
-    session.add(user)
-    session.commit()
+    #session = Session()
+    #session.add(user)
+    #session.commit()
 
     videos_list = json_data['items']
+    return json_data
 
+def printVideosList(videos_list):
     ids = []
     titles = []
 
     for i in videos_list:
         ids.append(i['id']['videoId'])
         titles.append(i['snippet']['title'])
-    return render_template('test.html', len = len(ids), video_ids = ids, vid_titles = titles)
+        return render_template('test.html', len = len(ids), video_ids = ids, vid_titles = titles)
+
+def GetVideosList():
+    videos = getVideosDataFromYT(None)
+    key = 'nextPageToken'
+
+    while(videos.get(key) is not None):
+        videos.update(getVideosDataFromYT(videos['nextPageToken']))
+        printVideosList(videos)
